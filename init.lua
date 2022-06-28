3528a744725c39cedfca155729c358778883b6cb
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-28 13:26:57 +0530

    remove: removed unused imports

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require "user.options"
 require "user.keymaps"
 require "user.plugins"
 require "user.cmp"
-require "user.lua"
 
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
