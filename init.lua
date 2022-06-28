a54e128ff325b72c6fb7f06ca0a3a5226de9bd79
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-28 13:25:16 +0530

    add: added imports for lsp for lua

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require "user.options"
 require "user.keymaps"
 require "user.plugins"
 require "user.cmp"
+require "user.lua"
 
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
