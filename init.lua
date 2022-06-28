94f21878621995b0b60faf4ecfaf5a4615292809
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-28 15:54:50 +0530

    add: added config support for telescope

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require "user.keymaps"
 require "user.plugins"
 require "user.cmp"
 require "user.lsp"
+require "user.telescope";
 
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
