4838b358474be8593f25a7abb86e73bf921601a9
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-28 13:30:19 +0530

    add: added plugin support for LSP

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,10 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
