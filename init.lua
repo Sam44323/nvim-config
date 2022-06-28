71d75669eea1bedb05fe628dbe2fa76b7da9690a
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-28 16:57:37 +0530

    add: added initial support for comments

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require "user.cmp"
 require "user.lsp"
 require "user.telescope";
 require "user.treesitter";
+require "user.comment";
 
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -88,6 +88,10 @@ return packer.startup(function(use)
    use "p00f/nvim-ts-rainbow"
    use "nvim-treesitter/playground"
 
+  -- Comment
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use "numToStr/Comment.nvim" -- Easily comment stuff
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
