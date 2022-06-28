079088ecb91a611e6ade74e68fe76c4fc130c1a8
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-28 13:38:19 +0530

    add: added updated data structure for lua

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require "user.options"
 require "user.keymaps"
 require "user.plugins"
 require "user.cmp"
+require "user.lsp"
 
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
